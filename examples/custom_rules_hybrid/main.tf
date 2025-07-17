0e96c184e77b1ad8bacc23d96af3b8cc0a888171
Rosthan Ngomssi <ngomsi@gmailcom>
2025-07-17 10:06:58 -0500

    bugs fixed

--- examples/custom_rules_hybrid/main.tf
+++ examples/custom_rules_hybrid/main.tf
@@ -1,28 +1,31 @@
 module "custom_rule_group" {
   source = "../../modules/waf_rule_group"
 
-  name        = "custom-rule-group-hybrid"
-  scope       = "REGIONAL"
-  description = "Hybrid definition rule group"
+  rule_group_name = "custom-rule-group-hybrid"
+  name           = "custom-rule-group-hybrid"
+  scope          = "REGIONAL"
+  capacity       = 100
+  metric_name    = "CustomRuleGroupHybrid"
   tags = {
     Environment = "dev"
   }
 
   custom_rules = [
     {
-      name         = "BlockSQLi"
-      priority     = 0
-      metric_name  = "SQLiRule"
-      type         = "sqli"
+      name           = "BlockSQLi"
+      priority       = 0
+      metric_name    = "SQLiRule"
+      type           = "sqli"
       field_to_match = "body"
-      action       = "block"
+      action         = "block"
     },
     {
-      name         = "BlockSQLInjection"
-      priority     = 1
-      statement    = "sqli_match_statement { field_to_match { body {} } text_transformations { priority = 0 type = \"NONE\" } }"
-      action       = "block"
-      metric_name  = "block_sqli"
+      name           = "BlockXSS"
+      priority       = 1
+      metric_name    = "XSSRule"
+      type           = "xss"
+      field_to_match = "uri_path"
+      action         = "block"
     }
   ]
 }
